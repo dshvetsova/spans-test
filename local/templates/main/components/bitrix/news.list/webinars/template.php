6f0f4592ba6f843e4c9225ae1f685a2273a18ac6
Daria Shvetsova <daria@greenmars.ru>
2021-09-04 00:14:55 +0300

    Update previous: when empty

--- local/templates/main/components/bitrix/news.list/webinars/template.php
+++ local/templates/main/components/bitrix/news.list/webinars/template.php
@@ -5,17 +5,21 @@ if ($arParams['AJAX'] == 'Y') {
 ?>
 <div class="container bg-white">
 	<div class="row row-cols-3 p-3 mt-2">
-		<?foreach ($arResult['ITEMS'] as $item) : ?>
-			<div class="col">
-				<div class="p-3 rounded bg-light mx-2 py-4 mt-2">
-					<h3><?=$item['~NAME'];?></h3>
-					<?foreach ($item['DISPLAY_PROPERTIES']['TYPE']['LINK_ELEMENT_VALUE'] as $linkedValue) : ?>
-						<a><?=$linkedValue['~NAME']?></a>
-					<?endforeach;?>
-					<p><?=$item['DISPLAY_PROPERTIES']['DATE']['~VALUE']?></p>
+		<?if (empty($arResult['ITEMS'])) : ?>
+			<div class="col">К сожалению, ничего не найдено.</div>
+		<?else : ?>
+			<?foreach ($arResult['ITEMS'] as $item) : ?>
+				<div class="col">
+					<div class="p-3 rounded bg-light mx-2 py-4 mt-2">
+						<h3><?=$item['~NAME'];?></h3>
+						<?foreach ($item['DISPLAY_PROPERTIES']['TYPE']['LINK_ELEMENT_VALUE'] as $linkedValue) : ?>
+							<a><?=$linkedValue['~NAME']?></a>
+						<?endforeach;?>
+						<p><?=$item['DISPLAY_PROPERTIES']['DATE']['~VALUE']?></p>
+					</div>
 				</div>
-			</div>
-		<?endforeach;?>
+			<?endforeach;?>
+		<?endif;?>
 	</div>
 </div>
 <?if ($arParams['AJAX'] == 'Y') {
